023c2c4432613787d3ff233f85e0e3f880054cd9
Daniel Ochoa <dochoajohn@gmail.com>
2018-02-14 10:40:32 -0300

    feat: Add Search component
    This is the component who encapsulates the search feature of the app. When the component is mounted it will fetch the list data in order to get the current bookshelf detail. When the component is unmount, the parent search history is deleted in order to always start fresh on navigation. It introduces a debouncing on the input using react-debounce-input to avoid performing N request based on the length of the search term.

---
+++ src/Search.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { DebounceInput } from 'react-debounce-input'
+import PropTypes from 'prop-types'
+import Loading from './Loading'
+import ListedBook from './ListedBook'
+
+class Search extends Component {
+  componentDidMount() {
+    this.props.fetch();
+  }
+  
+  componentWillUnmount() {
+    this.props.cleanSearch();
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to="/" className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <DebounceInput minLength={2}
+                           placeholder="Search by title or author"
+                           debounceTimeout={300}
+                           onChange={this.onSearchTermChange} />
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.props.loading ? <Loading /> : this.renderResult()}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+
+  onSearchTermChange = (event) => {
+    const query = event.target.value.toLowerCase().trim();
+    this.props.search(query);
+  }
+
+  renderResult() {
+    if (this.props.books.length === 0) {
+      return <h3>There are no books that matches your search term</h3>;
+    }
+
+    return this.renderBooks();
+  }
+
+  renderBooks() {
+    return this.props.books.map(book => {
+      book.shelf = this.findShelf(book.id);
+      return <ListedBook key={book.id} 
+                         book={book}
+                         changeBookshelf={this.props.changeBookshelf} />;
+    });
+  }
+
+  findShelf(id) {
+    let shelf = 'none';
+
+    this.props.bookshelfDetails.forEach(shelfDetail => {
+      if (shelfDetail.ids.includes(id)) {
+        shelf = shelfDetail.shelf;
+        return false;
+      }
+    });
+
+    return shelf;
+  }
+}
+
+Search.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      authors: PropTypes.arrayOf(PropTypes.string).isRequired,
+      imageLinks: PropTypes.shape({
+        thumbnail: PropTypes.string.isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+  cleanSearch: PropTypes.func.isRequired,
+  fetch: PropTypes.func.isRequired,
+  search: PropTypes.func.isRequired,
+  changeBookshelf: PropTypes.func.isRequired,
+};
+
+export default Search;
